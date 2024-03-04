02bc22cddf0341884c9fb7d84e925ba2320fc0db
Louis <66969720+Louisj99@users.noreply.github.com>
2024-03-04 20:46:20 +0000

    added login directory and url

---
+++ loginAndRegistration/views.py
@@ -0,0 +1,15 @@
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from .forms import LoginForm
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(request, username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
