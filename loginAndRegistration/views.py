7fdc42a6feec9e84d844e4accd11b5f78edead7f
Matt <Matthew2.Mrozowski@live.uwe.ac.uk>
2024-03-11 03:08:33 +0000

    Added Registration page and basic navbar to base html file

--- loginAndRegistration/views.py
+++ loginAndRegistration/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
-
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def login_view(request):
     if request.method == 'POST':
@@ -13,6 +13,7 @@ def login_view(request):
         if user is not None:
             login(request, user)
             # Redirect to a success page or home page
+            messages.success(request, ("Log in Complete"))
             return redirect('home')
         else:
             # Return an error message or handle authentication failure
@@ -22,4 +23,21 @@ def login_view(request):
 
 
 def register(request):
-    return render(request, 'registration.html')
+    form= UserCreationForm()
+    #context = 
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #info for automatic log in
+            username=form.cleaned_data['username']
+            #password 1 as two options for password
+            password=form.cleaned_data['password1']
+            user = authenticate(username=username,password=password)
+            login(request, user)
+            messages.success(request, ("Registration Complete"))
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'registration.html',{'form':form})
