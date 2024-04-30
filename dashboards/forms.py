d35aa8c6667262d12bfec63dc2a8242cbc6904cc
louisj99 <louis.stockton@hotmail.co.uk>
2024-04-30 23:33:49 +0100

    added functionality to change a users group

--- dashboards/forms.py
+++ dashboards/forms.py
@@ -25,9 +25,27 @@ class InvoiceForm(forms.ModelForm):
         fields = ['patient', 'billing_datetime', 'amount', 'payment_status']
 
 
+from django.contrib.auth.models import Group
+
+from django import forms
+from django.contrib.auth.models import User, Group
+from django.contrib.auth.forms import UserChangeForm
+from django.contrib.admin.widgets import FilteredSelectMultiple
+
+
 class CustomUserChangeForm(UserChangeForm):
     email = forms.EmailField(required=True)
+    groups = forms.ModelMultipleChoiceField(
+        queryset=Group.objects.all(),
+        widget=FilteredSelectMultiple("Groups", is_stacked=False),
+        required=False
+    )
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'first_name', 'last_name')
+        fields = ('username', 'email', 'first_name', 'last_name', 'groups')
+
+    def __init__(self, *args, **kwargs):
+        super(CustomUserChangeForm, self).__init__(*args, **kwargs)
+        if self.instance.pk:
+            self.fields['groups'].queryset = Group.objects.exclude(id__in=self.instance.groups.values_list('id', flat=True))
